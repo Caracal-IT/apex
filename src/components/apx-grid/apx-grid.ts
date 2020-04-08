91b5d6479b31668dd966712e7b8daa23cd3e1f1c
Ettiene Mare <ettienemare@gmail.com>
2020-04-08 19:55:11 +0200

    Add Datasource to the grid.

--- src/components/apx-grid/apx-grid.ts
+++ src/components/apx-grid/apx-grid.ts
@@ -1,25 +1,38 @@
 import css from './apx-grid.scss';
+import { Context } from 'caracal_polaris/dist/types/model/context.model';
 
 export class Grid extends HTMLElement {
     private _table: HTMLTableElement;
+    private _content: HTMLDivElement;
 
     private _items = [];
-
+   
     caption: string;
-    
+    columns: Array<Column>;
+    items: []|string;
+    ctx: Context;
 
+    searchColumn = '';
+    searchDirection = 'asc';
+    
     constructor(){
         super();
 
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = `<style>${css}</style>`;
 
+        this._content = document.createElement('div');
+        this.shadowRoot.appendChild(this._content);
         this._table = document.createElement('table');
-        this.shadowRoot.appendChild(this._table);
+        this._content.appendChild(this._table);
     }
 
-    connectedCallback() {
-        this.initItems();
+    async connectedCallback() {
+        await this.initItems();
+        this.render();
+    }
+
+    private render() {
         this._table.innerHTML = '';
         this.createHeader();
         this.createBody();
@@ -31,21 +44,52 @@ export class Grid extends HTMLElement {
         header.appendChild(row);
         this._table.appendChild(header);
 
-        this.createColumn("Id", row);
-        this.createColumn("Name", row);
-        this.createColumn("Surname", row);
+        this.columns.forEach(c => this.createColumn(c, row));        
     }
 
-    private createColumn(text: string, row: HTMLTableRowElement){
+    private createColumn(column: any, row: HTMLTableRowElement){
         const col = document.createElement('th');
         row.appendChild(col);
-        col.textContent = text;
+
+        let sortIcon = '';
+
+        if(this.searchColumn === column.name) {
+            sortIcon = this.searchDirection === 'asc' ? '<span>&uarr;</span>' : '<span>&darr;</span>';
+        }
+
+        col.innerHTML = column.caption + " " + sortIcon;
+
+        col.addEventListener('click', () => {
+            if(this.searchColumn !== column.name)
+                this.searchDirection = 'desc';
+
+                this.searchColumn = column.name;
+
+            if(this.searchDirection === 'desc')
+                this.searchDirection = 'asc';
+            else
+                this.searchDirection = 'desc';
+
+            const order = this.searchDirection === 'desc' ? -1 : 1;
+            const a = this._items.sort(this.compare.bind(this, order, column.name));
+
+            this._items =  a;
+            this.render();
+
+        });
+    }
+
+    public compare(order: number, name: string, a: any, b: any) {        
+        if(a[name] > b[name]) return 1 * order;
+        if(a[name] < b[name]) return -1 * order;
+
+        return 0;
     }
 
     private createBody() {
         const body = document.createElement('tbody');
         this._table.appendChild(body);
-        
+
         this._items.forEach(i => this.createRow(i, body));
     }
 
@@ -53,9 +97,7 @@ export class Grid extends HTMLElement {
         const row = document.createElement('tr');
         body.appendChild(row);
 
-        this.createCell(item.id, row);
-        this.createCell(item.name, row);
-        this.createCell(item.surname, row);
+        this.columns.forEach(c => this.createCell(item[c.name], row)); 
     }
 
     private createCell(text: string, row: HTMLTableRowElement) {
@@ -64,15 +106,22 @@ export class Grid extends HTMLElement {
         cell.textContent = text;
     }
 
-    private initItems() {
-        for(let i = 1; i < 5; i++) {
-            this._items.push({
-                "id": i,
-                "name": `Name ${i}`,
-                "surname": `Surname ${i}`
-            });
+    private async initItems() {
+        if(typeof this.items === 'string') {
+            if(this.items.indexOf('/') === -1)
+                this._items = this.ctx.model.getValue(this.items);
+            else if(this.items.length > 0) 
+                this._items = await this.ctx.http.fetch({url: this.items, method: 'GET'});
         }
+
+        if(!this._items)
+            this._items = [];
     }
 }
 
 customElements.define('apx-grid', Grid);
+
+class Column {
+    cation: string;
+    name: string;
+}
