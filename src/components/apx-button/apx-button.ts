ccd0ebe569acd843aca81b09aba639a613832176
Ettiene Mare <ettienemare@gmail.com>
2020-04-09 09:07:08 +0200

    Add default button

--- src/components/apx-button/apx-button.ts
+++ src/components/apx-button/apx-button.ts
@@ -5,10 +5,12 @@ import css from './apx-button.scss';
 export class Button extends HTMLElement {
     private _button: HTMLButtonElement;
     private _buttonHandler: any;
+    private _windowHandler: any;
 
     caption: string;
     ctx: Context;
     next: string;
+    isDefault = false;
 
     constructor(){
         super();
@@ -19,17 +21,29 @@ export class Button extends HTMLElement {
         this._button = document.createElement('button');
         this.shadowRoot.appendChild(this._button);
         this._buttonHandler = this.buttonHandler.bind(this);
+        this._windowHandler = this.windowHandler.bind(this);
     }
 
     connectedCallback() {
         this._button.textContent = this.caption;
         this._button.addEventListener('click', this._buttonHandler);
+
+        if(this.isDefault)
+            window.addEventListener('keypress', this._windowHandler);
     }
 
     disconnectedCallback() {
         this._button.removeEventListener('click', this._buttonHandler);
+
+        if(this.isDefault)
+            window.removeEventListener('keypress', this._windowHandler);
     }
     
+    private windowHandler(event: KeyboardEvent) {
+        if (event.key === 'Enter') 
+            this.buttonHandler();
+    }
+
     private buttonHandler() {
         this.ctx.wf.goto(this.next);
     }
