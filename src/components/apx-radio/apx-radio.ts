e632a29851c356785da684c365d00b77d7cf9e8a
Ettiene Mare <ettienemare@gmail.com>
2020-04-09 20:01:59 +0200

    Add radio button

--- src/components/apx-radio/apx-radio.ts
+++ src/components/apx-radio/apx-radio.ts
@@ -1,10 +1,17 @@
 import css from './apx-radio.scss';
 
 export class Radio extends HTMLElement {
-    private _label: HTMLSpanElement;
-
+    private _input: HTMLInputElement;
+    private _label: HTMLLabelElement;
+    private _textLabel: HTMLLabelElement;
+    
+    id: string;
+    name: string;
     caption: string;
     htmlFor: string;
+
+    get value() { return this._input.value; }
+    set value(value) { this._input.value = value; }
 
     constructor(){
         super();
@@ -12,12 +19,23 @@ export class Radio extends HTMLElement {
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = `<style>${css}</style>`;
 
-        this._label = document.createElement('span');
+        this._input = document.createElement('input');
+        this._input.type = "radio";
+        this.shadowRoot.appendChild(this._input);
+
+        this._label = document.createElement('label');
         this.shadowRoot.appendChild(this._label);
+
+        this._textLabel = document.createElement('label');
+        this.shadowRoot.appendChild(this._textLabel);
     }
 
     connectedCallback() {
-        this._label.textContent = this.caption;
+        this._input.id = this.id;
+        this._input.name = this.name;
+        this._label.htmlFor = this.id;
+        this._textLabel.htmlFor = this.id;
+        this._textLabel.textContent = this.caption;
     }
 }
 
