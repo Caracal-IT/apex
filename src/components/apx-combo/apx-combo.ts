34b51c8f138c2eb2d3bdab797143b59e20a487a4
Ettiene Mare <ettienemare@gmail.com>
2020-04-10 12:09:19 +0200

    refactor the combo

--- src/components/apx-combo/apx-combo.ts
+++ src/components/apx-combo/apx-combo.ts
@@ -6,69 +6,34 @@ export class Combo extends HTMLElement {
     private _label: HTMLLabelElement;
     private _combo: HTMLDivElement;
     private _input: HTMLInputElement;
+    private _icon: HTMLDivElement;
     private _itemContainer: HTMLUListElement;
+    private _isOpen = false;
+
+    private _inputFocusHandler: EventListener;
+    private _inputHandler: EventListener;
+    private _iconClickHandler: EventListener;
+    private _comboClickHandler: EventListener;
 
     caption: string;
     ctx:Context;
     items: [];
 
-   get value() { console.log('getValue'); return this._input.value; }
+   get value() { return this._input.value; }
    set value(value) { this._input.value = value; }
 
     constructor(){
         super();
 
-        let isOpen = false;
-
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = `<style>${css}</style>`;
 
-        this._container = document.createElement('div');
-        this._container.className = 'container';
-        this._container.tabIndex = 0;
-        this.shadowRoot.appendChild(this._container);
-
-        this._label = document.createElement('label');
-        this._container.appendChild(this._label);
-
-        this._combo = document.createElement('div');
-        this._combo.className = 'combo';
-        this._combo.tabIndex = 0;
-        
-        this._container.appendChild(this._combo);
-
-        this._input = document.createElement('input');
-        this._combo.appendChild(this._input);
-
-        const arrow = document.createElement('div');
-        arrow.className = 'arrow';
-
-        arrow.innerHTML = '<div>&#x25BC;</div>';
-        this._combo.appendChild(arrow);
-        arrow.addEventListener('click', () => {
-            if(isOpen === true)
-                this._container.focus();
-            else {
-                this._input.focus();
-                return;
-            }
-
-            isOpen = !isOpen;
-        });
-
-        this._input.addEventListener('focus', () => {
-            isOpen = true;
-        });
-
-        this._itemContainer = document.createElement('ul');
-        this._combo.appendChild(this._itemContainer);
-
-        this._itemContainer.addEventListener('click', (e: any) => {
-            this.value = e.target.textContent;//e.target.dataset.value;
-            this.dispatchEvent(new Event('input'));
-            this._container.focus();
-            isOpen = false;
-        });
+        this.createCombo();
+       
+        this._comboClickHandler = this.comboClickHandler.bind(this);
+        this._iconClickHandler = this.iconClickHandler.bind(this);
+        this._inputFocusHandler = this.inputFocusHandler.bind(this);
+        this._inputHandler = this.inputHandler.bind(this);
     }
 
     connectedCallback() {
@@ -76,11 +41,105 @@ export class Combo extends HTMLElement {
         this._input.id = this.id;
 
         this._itemContainer.innerHTML = '';
-
         this.items.forEach(this.createItem.bind(this));
+
+        this._itemContainer.addEventListener('click', this._comboClickHandler);
+        this._icon.addEventListener('click', this._iconClickHandler);
+        this._input.addEventListener('focus', this._inputFocusHandler);
+        this._input.addEventListener('input', this._inputHandler);
     }
 
-    createItem(item: any) {
+    disconnectedCallback() {
+        this._itemContainer.removeEventListener('click', this._comboClickHandler);
+        this._icon.removeEventListener('click', this._iconClickHandler);
+        this._input.removeEventListener('focus', this._inputFocusHandler);
+    }
+
+    private inputHandler() {
+
+    }
+
+    private comboClickHandler(event: any) {
+        this.value = event.target.textContent;//e.target.dataset.value;
+        this.dispatchEvent(new Event('input'));
+        this._container.focus();
+        this._isOpen = false;
+    }
+
+    private iconClickHandler() {
+        if(this._isOpen === true)
+            this._container.focus();
+        else {
+            this._input.focus();
+            return;
+        }
+
+        this._isOpen = !this._isOpen;
+    }
+
+    private inputFocusHandler() {
+        this._isOpen = true;
+    }
+
+    private createCombo() {
+        this.createContainer();
+        this.createCaption();
+        this.createComboInput();
+    }
+
+    private createContainer() {
+        this._container = document.createElement('div');
+        this._container.className = 'container';
+        this._container.tabIndex = 0;
+        this.shadowRoot.appendChild(this._container);
+    }
+
+    private createCaption() {
+        this._label = document.createElement('label');
+        this._container.appendChild(this._label);
+    }
+
+    private createComboInput() {
+        this.createComboContainer();
+        this.createInput();
+        this.createIcon();
+        this.createLookupItems();
+    }
+
+    private createComboContainer(){
+        this._combo = document.createElement('div');
+        this._combo.className = 'combo';
+        this._combo.tabIndex = 0;
+        
+        this._container.appendChild(this._combo);
+    }
+
+    private createInput() {
+        this._input = document.createElement('input');
+        this._combo.appendChild(this._input);
+    }
+
+    private createIcon(){
+        this._icon = document.createElement('div');
+        this._icon.className = 'arrow';
+
+        this._icon.innerHTML = '<div>&#x25BC;</div>';
+        this._combo.appendChild(this._icon);
+    }
+
+    private createLookupItems() {
+        this._itemContainer = document.createElement('ul');
+        this._combo.appendChild(this._itemContainer);
+
+        this._itemContainer.addEventListener('click', (e: any) => {
+            this.value = e.target.textContent;//e.target.dataset.value;
+            this.dispatchEvent(new Event('input'));
+            this._container.focus();
+            this._isOpen = false;
+        });
+    }
+
+    private createItem(item: any) {
         const li = document.createElement('li');
         li.dataset.value = item.value;
         li.textContent = this.ctx.model.getInterpolatedValue(item.caption);
