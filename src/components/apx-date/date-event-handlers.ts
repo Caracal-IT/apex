f5845d66d1ce2a047f4890b226a49e96ce8bd6a4
Ettiene Mare <ettienemare@gmail.com>
2020-04-12 19:36:27 +0200

    refactor popup events

--- src/components/apx-date/date-event-handlers.ts
+++ src/components/apx-date/date-event-handlers.ts
@@ -12,76 +12,18 @@ export class DateEventHandler {
     handle(event: any){
         this._event = event;
         this.cancelBubble();
-        
-        if(event.target.dataset.action === 'month') {
-            this.builder.buildMonths();
-            return;
+
+        switch (this._event.target.dataset.action) {
+            case 'month':  return this.builder.buildMonths();;
+            case 'select-month': return this.selectMonth();
+            case 'year': return this.builder.buildYears();
+            case 'set-year': return this.selectYear();
+            case 'clear-label': return this.clearDate();
+            case 'today-label': return this.setToday();
+            case 'left-arrow': return this.goToMonth(-1);
+            case 'right-arrow': return this.goToMonth(1);
+            default: return this.selectDate();
         }
-
-        if(event.target.dataset.action === 'select-month') {
-            const month = event.target.dataset.month;
-
-            const date = new Date(this.builder.date.getFullYear(), month, 1);
-            this.builder.buildDays(this.container._value, date);
-            return;
-        }
-
-        if(event.target.dataset.action === 'year') { 
-            this.builder.buildYears();
-            return; 
-        }
-
-        if(event.target.dataset.action === 'set-year') { 
-            const year = this.container.shadowRoot.querySelector('input').value;
-            
-            if(!/^\d{4}$/.test(year))
-                return;
-
-            const date = new Date(+year, this.builder.date.getMonth(), 1);
-            this.builder.buildDays(this.container._value, date);
-            return; 
-        }
-
-        if(event.target.dataset.action === 'clear-label') {
-            this.container.value = '';
-            this.popup.classList.add("hidden");
-            this.container.dispatchEvent(new Event('input'));
-            return;
-        }
-
-        if(event.target.dataset.action === 'today-label'){
-            const date = new Date();
-            this.container.value = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
-            this.popup.classList.add("hidden");
-            this.container.dispatchEvent(new Event('input'));
-            return;
-        }
-
-        if(event.target.dataset.action === 'left-arrow' || event.target.dataset.action === 'right-arrow'){
-            const date = new Date(event.target.dataset.date);
-            let newDate = new Date(date.getFullYear(), date.getMonth() - 1, date.getDate());
-
-            if(event.target.dataset.action === 'right-arrow')
-                newDate = new Date(date.getFullYear(), date.getMonth() + 1, date.getDate());
-
-            this.builder.buildDays(this.container._value, newDate);
-
-            return;
-        }
-
-        if(!event.target.dataset.date)
-            return;
-
-        const selected = this.popup.querySelector('.selected');
-
-        if(selected)
-            selected.classList.remove('selected');
-
-        event.target.classList.add('selected');
-
-        this.container.value = event.target.dataset.date;
-        this.popup.classList.add("hidden");
-        this.container.dispatchEvent(new Event('input'));
     }
 
     private cancelBubble() {
@@ -89,5 +31,64 @@ export class DateEventHandler {
 
         if(parent && parent.contains(this._event.target))
             this._event.cancelBubble = true;
+    }
+
+    private selectMonth() {
+        const month = this._event.target.dataset.month;
+
+        const date = new Date(this.builder.date.getFullYear(), month, 1);
+        this.builder.buildDays(this.container._value, date);
+    }
+
+    private selectYear() {
+        const year = this.container.shadowRoot.querySelector('input').value;
+            
+        if(!/^\d{4}$/.test(year))
+            return;
+
+        const date = new Date(+year, this.builder.date.getMonth(), 1);
+        this.builder.buildDays(this.container._value, date);
+    }
+
+    private clearDate() {
+        this.container.value = '';
+        this.popup.classList.add("hidden");
+        this.container.dispatchEvent(new Event('input'));
+    }
+
+    private setToday() {
+        const date = new Date();
+        this.container.value = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+        this.popup.classList.add("hidden");
+        this.container.dispatchEvent(new Event('input'));
+    }
+
+    private goToMonth(direction: number) {
+        const date = new Date(this._event.target.dataset.date);
+        let newDate = new Date(date.getFullYear(), date.getMonth() + direction, date.getDate());
+
+        this.builder.buildDays(this.container._value, newDate);
+    }
+
+    private selectDate() {
+        if(!this._event.target.dataset.date)
+            return;
+
+        this.clearSelected();
+        this._event.target.classList.add('selected');
+        this.setDate();
+    }
+
+    private clearSelected() {
+        const selected = this.popup.querySelector('.selected');
+
+        if(selected)
+            selected.classList.remove('selected');
+    }
+
+    private setDate() {
+        this.container.value = this._event.target.dataset.date;
+        this.popup.classList.add("hidden");
+        this.container.dispatchEvent(new Event('input'));
     }
 }
